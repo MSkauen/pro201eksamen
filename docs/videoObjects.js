6e161e7675e45a9e8da821dbc8940ffe83b26442
martinsorhus <sorhus.martin@gmail.com>
2020-12-16 06:10:37 +0100

    opprettet to arrays med objekter for videospilleren

---
+++ docs/videoObjects.js
@@ -0,0 +1,151 @@
+const videoObjectsForSunbellSmart = [
+    {
+        numberInLine: 1,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11110,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 2,
+        title: "removing the panel",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11111,
+        playIcon: "img/play.png"
+    },    
+    {
+        numberInLine: 3,
+        title: "fixing the buttons",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11112,
+        playIcon: "img/play.png"
+    },    
+    {
+        numberInLine: 4,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11113,
+        playIcon: "img/play.png"
+    },    
+    {
+        numberInLine: 5,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11114,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 6,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11115,
+        playIcon: "img/play.png"
+    },    {
+        numberInLine: 7,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11116,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 8,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11117,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 9,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11118,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 10,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11119,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 11,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11120,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 12,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11121,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 13,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 11122,
+        playIcon: "img/play.png"
+    }
+];
+
+const videoObjectsForMoveSmart = [
+    {
+        numberInLine: 1,
+        title: "removing the panel",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 22220,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 2,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 22221,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 3,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 22222,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 4,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 22223,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 5,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 22224,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 6,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 22225,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 7,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 22226,
+        playIcon: "img/play.png"
+    },
+    {
+        numberInLine: 8,
+        title: "dissasembling the battery",
+        videoSource: "https://www.youtube.com/embed/PK7g180smjo",
+        id: 22227,
+        playIcon: "img/play.png"
+    }
+]
